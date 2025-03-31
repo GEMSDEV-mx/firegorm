183088201b7537fb72df890186783c104434b7a2
Carlos Covarrubias <ccovarrubias@gemsdev.mx>
2025-03-31 15:50:42 -0600

    feat: added utils ExtractFilters

--- utils.go
+++ utils.go
@@ -1,6 +1,8 @@
 package firegorm
 
 import (
+	"strings"
+
 	"cloud.google.com/go/firestore"
 	"github.com/google/uuid"
 )
@@ -24,4 +26,31 @@ func updatesToFirestoreUpdates(updates map[string]interface{}) []firestore.Updat
 	}
 	Log(INFO, "Converted updates to Firestore format: %+v", firestoreUpdates)
 	return firestoreUpdates
+}
+
+// ExtractFilters converts a map of query parameters (key-value strings) into a filters map.
+// Any parameter value that contains a comma is split into a []string.
+// The caller can pass a slice of keys to exclude from processing.
+func ExtractFilters(params map[string]string, exclude []string) map[string]interface{} {
+    filters := make(map[string]interface{})
+    // Build an exclusion lookup.
+    excludeMap := make(map[string]bool)
+    for _, key := range exclude {
+        excludeMap[key] = true
+    }
+    for key, value := range params {
+        if excludeMap[key] {
+            continue
+        }
+        if strings.Contains(value, ",") {
+            parts := strings.Split(value, ",")
+            for i, part := range parts {
+                parts[i] = strings.TrimSpace(part)
+            }
+            filters[key] = parts
+        } else {
+            filters[key] = value
+        }
+    }
+    return filters
 }
